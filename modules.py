0c5a6861a3f238e2310984711c6bb21818a57f63
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 13:43:38 -0500

    Update modules.py

--- modules.py
+++ modules.py
@@ -2,4 +2,13 @@
 
 import math
 import random
+import sys
 
+r = sys.argv[1]
+radius = int(r)
+
+volume = (4 * math.pi * (radius ** 3))/3
+print(f"\nVolume of sphere with radius {radius} = {volume:0.2f} sq units")
+
+num = random.randint(1,10)
+print(f"{num}! = {math.factorial(num)}\n")
