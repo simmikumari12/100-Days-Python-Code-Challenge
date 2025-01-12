433c766c0b50537278cf0546be4cd4befb755130
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 11:49:11 -0500

    Update change.py

--- change.py
+++ change.py
@@ -1,13 +1,8 @@
 cents = int(input("Please Enter a number of cents: "))
 
 quarters = cents // 25
-print("quarters", quarters)
 dimes = (cents - quarters * 25)//10
-print("dimes:",dimes)
-
 nickels = (cents - quarters * 25 - dimes * 10)//5
-print("nickels:",nickels)
-
 pennies = (cents - quarters * 25 - dimes * 10 - nickels * 5)//1
-print("pennies:",pennies)
 
+print(f"Coins: {quarters} quarters, {dimes} dimes, {nickels} nickels, {pennies} pennies")
