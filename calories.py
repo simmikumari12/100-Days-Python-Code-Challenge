03757e8030eb6202ac9838f32dcbe8a413389c3a
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 02:31:29 -0500

    Update calories.py

--- calories.py
+++ calories.py
@@ -1,1 +1,8 @@
 age = int(input("Age = "))
+weight = int(input("Weight = "))
+heart_rate = int(input("Heart Rate = "))
+time = int(input("Time = "))
+
+calories = ((age * 0.2757 + weight * 0.03295 + heart_rate * 1.0781 - 75.4991) * time)/ 8.368
+
+print(f"Calories burned: {calories:.2f}")
