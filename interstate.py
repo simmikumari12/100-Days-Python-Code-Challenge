4301975e9e1a0a335c2b7ef946fcb1984b39b6ad
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 18:43:05 -0500

    Update interstate.py

--- interstate.py
+++ interstate.py
@@ -1,16 +1,23 @@
+import sys
+
 number = int(input("Please enter an interstate number: "))
 
+def direction_decider(a):
+    if a % 2 == 0:
+        return "east/west"
+    else:
+        return "north/south"
+
 if 1 < number < 99:
-    pass
+    print(f"I-{number} is primary, going {direction_decider(number)}")
 
 
 if 100 < number < 999:
     last_digits = number % 100 
+
     if last_digits == 00:
         print(f"{number} is not a valid interstate highway number.")
-    if last_digits % 2 == 0:
-        direction = "east/west"
-    else:
-        direction = "north/south"
-    print(f"I-{number} is auxiliary, serving I-{last_digits}, going {direction}")
+        sys.exit()
+
+    print(f"I-{number} is auxiliary, serving I-{last_digits}, going {direction_decider(last_digits)}")
 
