b5669108a80cc84d14f7408c304cf25a1d2514c5
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 11:47:16 -0500

    Update change.py

--- change.py
+++ change.py
@@ -8,5 +8,6 @@ print("dimes:",dimes)
 nickels = (cents - quarters * 25 - dimes * 10)//5
 print("nickels:",nickels)
 
-pennies = (cents - quarters * 25 - dimes * 10 - nickels * 5)//5
+pennies = (cents - quarters * 25 - dimes * 10 - nickels * 5)//1
 print("pennies:",pennies)
+
