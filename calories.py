75dfab9c2aa78515af8a26f1920131954ceca0ef
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 11:30:12 -0500

    Update calories.py

--- calories.py
+++ calories.py
@@ -5,22 +5,23 @@ while age <= 0:
     try:
         age = int(input("Age = "))
     except:
-        print("Invalid Age!")
+        pass
 
 weight = 0
 while weight <= 0:
     try:
         weight = int(input("Weight = "))     
     except:
-        print("Invalid age!")
+        pass
+heart_rate = int(input("Heart Rate = "))
+time = int(input("Time = "))
 
-while True:
-    try:
-        weight = int(input("Weight = "))     
-    except:
-        print("Invalid Weight!")
-        heart_rate = int(input("Heart Rate = "))
-        time = int(input("Time = "))
- 
-# calories = ((age * 0.2757 + weight * 0.03295 + heart_rate * 1.0781 - 75.4991) * time)/ 8.368
-# print(f"Calories burned: {calories:.2f}")
+
+calories = ((age * 0.2757 + weight * 0.03295 + heart_rate * 1.0781 - 75.4991) * time)/ 8.368
+
+if calories > 0:   
+    print(f"Calories burned: {calories:.2f}")
+elif calories < 0:
+    print(f"Calories gained: {-calories:.2f}")
+else:
+    print(f"Calories: {calories:.2f}")
