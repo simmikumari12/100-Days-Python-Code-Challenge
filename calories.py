9404e8a2b8af025fc8ef545825e5655b61bc8e25
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 10:29:28 -0500

    Update calories.py

--- calories.py
+++ calories.py
@@ -1,8 +1,26 @@
-age = int(input("Age = "))
-weight = int(input("Weight = "))
-heart_rate = int(input("Heart Rate = "))
-time = int(input("Time = "))
+import sys
 
-calories = ((age * 0.2757 + weight * 0.03295 + heart_rate * 1.0781 - 75.4991) * time)/ 8.368
+age = 0
+while age <= 0:
+    try:
+        age = int(input("Age = "))
+    except:
+        print("Invalid Age!")
 
-print(f"Calories burned: {calories:.2f}")
+weight = 0
+while weight <= 0:
+    try:
+        weight = int(input("Weight = "))     
+    except:
+        print("Invalid age!")
+
+while True:
+    try:
+        weight = int(input("Weight = "))     
+    except:
+        print("Invalid Weight!")
+        heart_rate = int(input("Heart Rate = "))
+        time = int(input("Time = "))
+ 
+# calories = ((age * 0.2757 + weight * 0.03295 + heart_rate * 1.0781 - 75.4991) * time)/ 8.368
+# print(f"Calories burned: {calories:.2f}")
