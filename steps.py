080ec95e814ac25727605392ede0a549e57ca7f3
Sameer Kumar <simmikumari942@gmail.com>
2025-01-12 22:19:33 -0500

    coded text_filter file

--- steps.py
+++ steps.py
@@ -1,6 +1,7 @@
 def feet_to_steps(num_feet):
     num_steps = int(num_feet * 2.5)
     return num_steps
+
 def main():
     num_feet = int(input("Please enter the distance travelled in feets: "))
     print(feet_to_steps(num_feet),"steps")
