909cf04a10c73ce44e6c29b00d220d22a761788e
Sameer Kumar <simmikumari942@gmail.com>
2025-01-13 13:51:44 -0500

    Update SayMilitaryTime.py

--- SayMilitaryTime.py
+++ SayMilitaryTime.py
@@ -2,7 +2,39 @@ import sys
 from gtts import gTTS
 from preferredsoundplayer import soundplay
 
-military_time = sys.argv[1]
-accent = sys.argv[2]
-if len(sys.argv) > 3:
-    print("Too many arguments!")
+def main():
+    
+    if len(sys.argv) > 3:
+        print("Too many arguments!")
+        print("Usage: python3 SayMilitaryTime.py 1859 USA")
+        sys.exit()
+    if len(sys.argv) < 3:
+        print("Too few arguments!")
+        print("Usage: python3 SayMilitaryTime.py 1859 USA")
+        sys.exit()
+    try:    
+        military_time = int(sys.argv[1])
+    except ValueError:
+        print("Invalid 1st command line argument. Should contain only digits.")
+        sys.exit()
+    accent = sys.argv[2]
+
+    valid_accent = ["AUS", "IND", "USA"]
+    if accent not in valid_accent:
+        print("Invalid 2nd argument. Must be one of IND, AUS, USA")
+        sys.exit()
+    hours = military_time//100
+    minutes = military_time % 100
+
+    if minutes > 59:
+        print("Invalid 1st command line argument. Minutes cannot be more than 59")
+        sys.exit()
+    if hours > 23:
+        print("Invalid 1st command line argument. Hours cannot be more than 23.")
+        sys.exit()
+
+    print(convert_minutes(minutes))
+
+def convert_minutes(minutes):
+    pass
+	
