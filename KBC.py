d3a8816995658d0aea9670af01d8a7ddf3aef48c
Sameer Kumar <simmikumari942@gmail.com>
2025-01-13 23:25:50 -0500

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -3,7 +3,9 @@ def main():
     print("\nIn 2022, which team became the first to score more than 500 runs on the first day of Test Match?")
     option_row_one = ["A: India", "B: Australlia"]
     option_row_two = ["C: New Zealand", "D: England"]
-    print(option_row_one).center()
+    for option in option_row_one:
+
+        print(option)
     print(option_row_two)
 
 if __name__ == "__main__":
