c390c2e8c05983b0f375ad0b292dbe3d62214b37
Sameer Kumar <simmikumari942@gmail.com>
2025-01-13 23:15:53 -0500

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -1,1 +1,7 @@
 #create a program to display questions to the user like KBC.
+def main():
+    print("In 2022, which team became the first to score more than 500 runs on the first day of Test Match?")
+
+
+if __name__ == "__main__":
+    main()
