196ea362180503615fa0bfc7ebceb1107d7bf8c1
Sameer Kumar <simmikumari942@gmail.com>
2025-01-13 23:27:52 -0500

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -1,12 +1,14 @@
 #create a program to display questions to the user like KBC.
 def main():
-    print("\nIn 2022, which team became the first to score more than 500 runs on the first day of Test Match?")
-    option_row_one = ["A: India", "B: Australlia"]
-    option_row_two = ["C: New Zealand", "D: England"]
+    print("\nIn 2022, which team became the first to score more than 500 runs on the first day of Test Match?\n")
+    option_row_one = ["A:India", "B:Australlia"]
+    option_row_two = ["C:New Zealand", "D:England"]
     for option in option_row_one:
+        print(option, end=" ")
+    print("\n")
+    for option in option_row_two:
+        print(option, end=" ")
 
-        print(option, end="")
-    print(option_row_two)
 
 if __name__ == "__main__":
     main()
