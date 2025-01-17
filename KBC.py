30b527aedb0c8e888c3d9aea8e04d973ee1bed4e
Sameer Kumar <simmikumari942@gmail.com>
2025-01-17 10:13:13 -0500

    Update KBC.py

--- KBC.py
+++ KBC.py
@@ -2,7 +2,7 @@
 import sys
 def main():
     questions = {"1: In 2022, which team became the first to score more than 500 runs on the first day of Test Match?": ["A:India", "B:Australlia", "C:New Zealand", "D:England"],
-                "2: Which of the following countries is the world's largest producer of saffron?": ["A:Spain","B:Iran", "C:India" "D:Greece"],
+                "2: Which of the following countries is the world's largest producer of saffron?": ["A:Spain","B:Iran", "C:India", "D:Greece"],
                 "3: Which god is also known as ‘Gauri Nandan’?": ["A:Agni","B:Indra","C.Hanuman","D:Ganesha"],
                 "4: What does not grow on tree according to a popular Hindi saying?": ["A:Money","B:Flowers","C:Leaves","D:Fruits"],
                 "5: Which city is known as the Pink City of India?": ["A:Banglore","B:Maysore","C:Jaipur", "D:Kochi"],
@@ -13,17 +13,25 @@ def main():
                 "10: Where is India Gate located?": ["A:Agra","B:Punjab","C:Mumbai","D:New Delhi"],
                 }
     i = 0
+    amt = 1000
     answers = {1:"D", 2:"B", 3:"D",4:"A", 5:"C", 6:"A", 7:"A", 8:"B", 9:"A", 10:"D"}
-    for question in questions:
-        print(question) 
-        print(questions[question]) #prints option
-        i = i + 1
-        user_answer = input("") 
-        if user_answer == answers[i]:
-            continue
-        else:
-            print("Wrong Answer!")
-            sys.exit()
+    try:
+        for question in questions:
+            print(f"Question for {amt}")
+            print(question) 
+            print(questions[question]) #prints option
+            i = i + 1
+            user_answer = input("") 
+            if user_answer == answers[i]:
+                amt = amt + 2000
+                continue
+            else:
+                print("Wrong Answer!")
+                print(f"Amount you are taking home: {amt}")
+                sys.exit()
+    except KeyboardInterrupt:
+        sys.exit()
+
  
 if __name__ == "__main__":
     main()
