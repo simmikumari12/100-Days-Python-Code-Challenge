ab00c8e296231133592f645891cb382a6c1b34b0
Sameer Kumar <simmikumari942@gmail.com>
2025-01-17 11:20:16 -0500

    Update codeDecode.py

--- codeDecode.py
+++ codeDecode.py
@@ -2,23 +2,27 @@ import random
 import string
 
 def main():
-    ans = input("Code or Decode?").lower()
+    ans = input("Code or Decode? ").lower()
     if ans == "code":
-        msg = input("Enter your message: ")
-        if len(msg) <= 3:
-            print(f"{msg[::-1]}")
-        elif len(msg) > 3:
-            first_letter = msg[0]
-            coded_value = msg[1:]
-            characters = string.ascii_letters
-            rc1 = random.choice(characters)
-            rc2 = random.choice(characters)
-            rc3 = random.choice(characters)
-            rc4 = random.choice(characters)
-            rc5 = random.choice(characters)
-            rc6 = random.choice(characters)
-            print(f"Coded Message:{rc1}{rc2}{rc3}{coded_value}{first_letter}{rc4}{rc5}{rc6}")
+        print(code())
+    elif ans == "decode":
+        decode()
+        
+def code():
+    msg = input("Enter your message: ")
+    if len(msg) <= 3:
+        return f"{msg[::-1]}"
+    elif len(msg) > 3:
+        first_letter = msg[0]
+        coded_value = msg[1:]
+        random_choice = []
+        characters = string.ascii_letters
+        for i in range(6):
+            random_choice.append(random.choice(characters))
+        return f"Coded Message:{random_choice[0]}{random_choice[1]}{random_choice[2]}{coded_value}{first_letter}{random_choice[3]}{random_choice[4]}{random_choice[5]}"
 
+def decode():
+    pass
 
 
 if __name__ == "__main__":
