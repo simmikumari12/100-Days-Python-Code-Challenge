b7eebcc00c73c10c46dee4ffdf0274296482a230
Sameer Kumar <simmikumari942@gmail.com>
2025-01-18 18:43:38 -0500

    Update snakeWaterGame.py

---
+++ snakeWaterGame.py
@@ -0,0 +1,16 @@
+import random 
+
+
+def main():
+    user = input("Choose between Snake, Gun and Water")
+    possible_values = ["Snake", "Water", "Game"]
+
+    
+
+
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
