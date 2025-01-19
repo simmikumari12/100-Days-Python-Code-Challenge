b3c7e5fed5ed53cf71bbac7812220b6eaedbac30
Sameer Kumar <simmikumari942@gmail.com>
2025-01-18 19:48:33 -0500

    Update snakeWaterGame.py

--- snakeWaterGame.py
+++ snakeWaterGame.py
@@ -2,15 +2,42 @@ import random
 
 
 def main():
-    user = input("Choose between Snake, Gun and Water")
-    possible_values = ["Snake", "Water", "Game"]
+    user = input("Choose between Snake, Gun and Water ").lower()
+    possible_values = {"snake": 1, "water": 2, "gun": 3}
+    value = possible_values[user]
+    machine = random.randint(1,4)
+    print(machine)
 
-    
+    if machine == 1: # machine gave snake
+        if value == 1:
+            print("Game Draw!")
+        elif value == 2:
+            print("You Lost!")
+        elif value == 3:
+            print("You Won!")
+        else:
+            print("Invalid Input")
 
 
+    if machine == 2: #machine gave water
+        if value == 2:
+            print("Game Draw!")
+        elif value == 3:
+            print("You Lost!")
+        elif value == 1:
+            print("You Won!")
+        else:
+            print("Invalid Input")
 
-
-    pass
+    if machine == 3: #machine gave gun
+        if value == 3:
+            print("Game Draw!")
+        elif value == 1:
+            print("You Lost!")
+        elif value == 2:
+            print("You Won!")
+        else:
+            print("Invalid Input")    
 
 if __name__ == "__main__":
     main()
