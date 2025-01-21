13a4835402067fc76b6d5cfd3f355ed2c57fb05d
Sameer Kumar <simmikumari942@gmail.com>
2025-01-21 00:46:23 -0500

    Update speeding.py

--- speeding.py
+++ speeding.py
@@ -1,23 +1,26 @@
 import sys
 
-speed_limit = int(input("Please enter the speed limit for the road: "))
-recorded_speed = int(input("Please enter the vehicle's recorded speed: "))
+def main():
+    speed_limit = int(input("Please enter the speed limit for the road: "))
+    recorded_speed = int(input("Please enter the vehicle's recorded speed: "))
 
-conditional_value = speed_limit - recorded_speed
+    conditional_value = speed_limit - recorded_speed
 
-if 10 < conditional_value :
-    ticket_price = 50
-elif 6 < conditional_value <= 20:
-    ticket_price = 75
-elif 21 <= conditional_value <40:
-    print(conditional_value)
-    ticket_price = 150
-elif conditional_value >= 40:
-    ticket_price = 300
-else:
-    print("There is no fine")
-    sys.exit()
-print(f"The speeding fine is ${ticket_price}.")
+    if 10 < conditional_value :
+        ticket_price = 50
+    elif 6 < conditional_value <= 20:
+        ticket_price = 75
+    elif 21 <= conditional_value <40:
+        print(conditional_value)
+        ticket_price = 150
+    elif conditional_value >= 40:
+        ticket_price = 300
+    else:
+        print("There is no fine")
+        sys.exit()
+    print(f"The speeding fine is ${ticket_price}.")
 
+if __name__ == "__main__":
+    main()
 
 #too slow speed will make traffic jam so if the speed limit is 23 and the driving speed is 3, there will be fine of $50
