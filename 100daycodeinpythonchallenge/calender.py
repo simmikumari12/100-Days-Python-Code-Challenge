580c978d80a7e4140998b9fd7b98694d802e9f1b
Sameer Kumar <simmikumari942@gmail.com>
2025-01-21 00:08:15 -0500

    Update calender.py

--- 100daycodeinpythonchallenge/calender.py
+++ 100daycodeinpythonchallenge/calender.py
@@ -31,28 +31,31 @@ def classifier(my_month, d):
             print(f"{my_month} {d} is autumn in the southern hemisphere.")
         else:
             print(f"{my_month} {d} is winter in the southern hemisphere.")
-  
-    if 3 < m < 6: 
-        print(f"{my_month} {d} is autumn in the southern hemisphere.")
-
-
 
     if m == 9:
         if d > 21:
             print(f"{my_month} {d} is spring in the southern hemisphere.")
         else:
-            print(f"{my_month} {d} is winter in the southern hemisphere.")
-
-
-
+            print(f"{my_month} {d} is winter in the southern hemisphere.")        
+  
     if m == 12 :
         if d < 20:
             print(f"{my_month} {d} is  spring in the southern hemisphere.")
         else:
             print(f"{my_month} {d} is  summer in the southern hemisphere.")
 
+    if 3 < m < 6: 
+        print(f"{my_month} {d} is autumn in the southern hemisphere.")
+
     if 6 < m < 9: 
         print(f"{my_month} {d} is winter in the southern hemisphere.")
+
+    if 9 < m < 12: 
+        print(f"{my_month} {d} is spring in the southern hemisphere.")
+    
+    if 1 <= m < 3: 
+        print(f"{my_month} {d} is summer in the southern hemisphere.")
+    
 
 
 
