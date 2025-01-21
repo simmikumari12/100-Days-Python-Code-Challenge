d897fedbf733bdee4a1a47a5411b76b44afb602c
Sameer Kumar <simmikumari942@gmail.com>
2025-01-21 00:45:08 -0500

    Update modules.py

--- modules.py
+++ modules.py
@@ -4,17 +4,21 @@ import math
 import random
 import sys
 
-r = sys.argv[1]
-try:
-    radius = int(r)  
-except ValueError:
-    print("\nInvalid Radius")
-else:
-    if radius > 0:
-        volume = (4 * math.pi * (radius ** 3))/3
-        print(f"\nVolume of sphere with radius {radius} = {volume:0.2f} sq units")
-    else:
+def main():
+    r = sys.argv[1]
+    try:
+        radius = int(r)  
+    except ValueError:
         print("\nInvalid Radius")
+    else:
+        if radius > 0:
+            volume = (4 * math.pi * (radius ** 3))/3
+            print(f"\nVolume of sphere with radius {radius} = {volume:0.2f} sq units")
+        else:
+            print("\nInvalid Radius")
 
-num = random.randint(1,10)
-print(f"{num}! = {math.factorial(num)}\n")
+    num = random.randint(1,10)
+    print(f"{num}! = {math.factorial(num)}\n")
+
+if __name__ == "__main__":
+    main()
