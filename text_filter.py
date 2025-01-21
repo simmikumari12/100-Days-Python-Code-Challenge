fb894df6d2842dccb6538e47505c7afdafa386eb
Sameer Kumar <simmikumari942@gmail.com>
2025-01-21 00:56:34 -0500

    Update text_filter.py

--- text_filter.py
+++ text_filter.py
@@ -2,8 +2,15 @@ def main():
     msg = input("Input Message: ")
     print(f"Output Message: {text_filter(msg)}")
 
-def text_filter(senten):
-    pass
+def text_filter(sentence):
+    banned_words = ["turkey", "dog", "fox", "cat", "chicken"]
+    updated = []
+    for word in sentence:
+        if word != banned_words:
+            updated.append(word)
+
+    print(word)
+
 
 if __name__ == "__main__":
     main()
