16837344e4e1d3f5d7749ea08c0c93a9b3e381e6
Sameer Kumar <simmikumari942@gmail.com>
2025-01-20 23:52:08 -0500

    Update calender.py

--- 100daycodeinpythonchallenge/calender.py
+++ 100daycodeinpythonchallenge/calender.py
@@ -1,6 +1,24 @@
 def my_input():
-    month = input("Please Enter the month: ")
+    month = input("Please Enter the month: ").title()
     day = input("Please Enter the day: ")
+    classifier(month, day)
 
-def classifier():
-    pass
+def classifier(my_month, my_day):
+    month = {
+        "January": 1,
+        "February": 2,
+        "March": 3,
+        "April": 4,
+        "May": 5,
+        "June": 6,
+        "July": 7,
+        "August": 8,
+        "September": 9,
+        "October": 10,
+        "November": 11,
+        "December": 12
+    }
+    print(month[my_month])
+    print(type(month[my_month]))
+
+my_input()
