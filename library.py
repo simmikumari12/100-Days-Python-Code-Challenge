2e6502be82c824537b650a458d90aff9a6860337
Sameer Kumar <simmikumari942@gmail.com>
2025-01-21 21:20:19 -0500

    Update library.py

--- library.py
+++ library.py
@@ -2,3 +2,12 @@ class Library:
     def __init__(self, no_of_books, books):
         self.no_of_books = no_of_books
         self.books = books
+
+    def print_books(self):
+        pass
+
+    def add_a_book(self):
+        pass
+
+    def num_books(self):
+        pass
