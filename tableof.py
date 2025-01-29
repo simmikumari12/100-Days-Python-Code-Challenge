de9beb22d30be48aff1ad97de4e262b37a3ec23e
Sameer Kumar <simmikumari942@gmail.com>
2025-01-29 10:19:00 -0500

    Update tableof.py

--- tableof.py
+++ tableof.py
@@ -1,14 +1,18 @@
 def main():
+
     while True:
         try:
-            num = int(input("Write table of: "))
+            num = int(input("\nTable of "))
+
             break
         except ValueError:
             print("Invalid Input")
+        
             
 
     for i in range(1, 11):
         print(f"{num} * {i} = {i * num}")
+    print("\n")
 
 if __name__ == "__main__":
     main()
