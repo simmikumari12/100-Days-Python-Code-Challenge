9c20eb6c4f7b0f492bc83339942019b25fd1804e
Sameer Kumar <simmikumari942@gmail.com>
2025-02-02 18:42:17 -0500

    update

--- multiplication_file.py
+++ multiplication_file.py
@@ -5,21 +5,27 @@ def main():
     read_from_file()
 
 def write_to_file():
-    file = open("numbers.txt", "w")
+    file1 = open("numbers.txt", "w")
     nums_to_write = input("Give numbers to write: ")
-    file.write(nums_to_write)
-    file.close()
+    file1.write(nums_to_write)
+    file1.close()
 
 def read_from_file():
-    file = open("numbers.txt", "r")
-    for nums in file:
-        a_list = nums.split(" ")
+    file2 = open("numbers.txt", "r")
+    for nums in file2:
+        a_list = nums.split(" ") # could have used file.writelines
     try:
-        print(reduce(lambda x, y: float(x) * float(y), a_list, 1))
+        value = reduce(lambda x, y: float(x) * float(y), a_list, 1)
+        print(value)
+        
     except:
         print("Invalid Input!")
+        
+    file2.close()
 
-    file.close()
+    file3 = open("numbers.txt", "a")
+    file3.write(f" = {value}")
+    file3.close()
 
 if __name__ == "__main__":
     main()
