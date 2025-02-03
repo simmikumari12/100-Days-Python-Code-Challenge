6800a9741fbd56f6be1532c32ee229d9bd3bc673
Sameer Kumar <simmikumari942@gmail.com>
2025-02-03 12:18:41 -0500

    code class_activity_2 file for attendence

---
+++ todos.py
@@ -0,0 +1,31 @@
+import sys 
+
+
+# Read file
+
+
+# Add Todo
+
+
+# Remove Todo
+
+# Save File
+
+# Print List
+
+
+print("\nHere's your ToDo List:\n")
+print("1. Ace TeleCom & Social Media Influencer Interview")
+print("2. Complete Raj Sunderaman's Assigment")
+print("3. Study for Chem Exam")
+print("4. Study for Math Exam")
+print("5. Start working on next Youtube Video,'Things to know before coming to GSU' or 'My first semester experience at college in USA' or 'Do's and Don't of SAT'or 'A day in my life in Spring semester/ winter vacation")
+print("6. Prepare for Summer Confrence Leadership Position Interview")
+print("8. Prepare for th RA Interview")
+
+#Print Commands
+
+print("\n*******************************************\n")
+print(f"To view ToDos: \n{sys.argv[0]}")
+print(f"\nTo add a ToDo:\n{sys.argv[0]} add \"Code CS50 Program\"\n")
+print(f"\nTo add or complete ToDo:\n{sys.argv[0]} remove 2\n")
