c721a40e932991789488b32f1ec4b0f29cfac1ea
Sameer Kumar <simmikumari942@gmail.com>
2025-02-07 16:55:31 -0500

    Update simmi_kumari_HW1.py

--- simmi_kumari_HW1.py
+++ simmi_kumari_HW1.py
@@ -1,8 +1,18 @@
 class Location:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
 
 class Car:
-    pass
+    def __init__(self, car_name, location, cost_per_mile):
+        self.car_name = car_name
+        self.cost_per_mile = cost_per_mile
+        Location.__init__(self, location)
+        
+
 
 class Passenger:
     pass
