908bb067c86b4c6f4ca0db3a695a6dd9e006c9d5
Sameer Kumar <simmikumari942@gmail.com>
2025-02-07 17:07:05 -0500

    Update simmi_kumari_HW1.py

--- simmi_kumari_HW1.py
+++ simmi_kumari_HW1.py
@@ -6,14 +6,31 @@ class Location:
     def __str__(self):
         return f"({self.x},{self.y})"
 
-class Car:
+class Car(Location):
     def __init__(self, car_name, location, cost_per_mile):
         self.car_name = car_name
         self.cost_per_mile = cost_per_mile
         Location.__init__(self, location)
         
-class Passenger:
-    pass
+    def __str__(self):
+        return f"[{self.car_name}, {self.location}, {self.cost_per_mile}]"
+    
+    def move_to(self,new_x, new_y):
+        return Location.__init__(new_x,new_y)
+
+class Passenger(Location):
+    def __init__(self, passenger_name, location):
+        self.passenger_name = passenger_name
+        Location.__init__(self, location)
+
+    def __str__(self):
+        return f"[{self.car_name}, {self.location}, {self.cost_per_mile}]"
+    
+    def move_to(self,new_x, new_y):
+        return Location.__init__(new_x,new_y)
+
 
 class RideSharingApp:
-    pass
+    def __init__(self):
+        cars = []
+        passengers = []
