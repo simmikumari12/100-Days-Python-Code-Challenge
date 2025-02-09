46bf8c64772a8154336874dae5af10275c479b15
Sameer Kumar <simmikumari942@gmail.com>
2025-02-09 16:13:34 -0500

    updated scourgify.py

--- scourgify.py
+++ scourgify.py
@@ -1,28 +1,27 @@
 import sys
 import csv
 
-
 # Checks Input Validity
 sys_len = len(sys.argv)
 if sys_len <= 2:
     sys.exit("Too few command-line arguments")
-
 if sys_len > 3:
     sys.exit("Too many command-line arguments")
-
 unordered_csv = sys.argv[1]
 ordered_csv = sys.argv[2]
-ls = []
+output_data = []
+
 try:
     with open(unordered_csv, "r") as f:
-        for line in f.readlines():
-            ls.append(line.split(","))
+        csv_data = csv.DictReader(f)
+        for row in csv_data:
+            last, first = row['name'].split(", ")
+            output_data.append({"first": first, "last": last, "house": row['house']})
 except FileNotFoundError:
-    sys.exit(f"Could not read {old_csv}")
+    sys.exit(f"Could not read {unordered_csv}")
 
-
-with open(ordered_csv, "a") as f2:
-    f2.write(",".join(ls[0]))
-    for i in range(1,len(ls)):
-        # f2.write(f'"{(ls[i][1]).replace('"',"").replace(' ','').strip()}, {(ls[i][0]).replace('"',"")}",{ls[i][2]}')
-        f2.write(f'{(ls[i][1]).replace('"',"").replace(' ','').strip()}, {(ls[i][0]).replace('"',"")}, {ls[i][2]}')
+with open(ordered_csv, "w") as f2:
+    writer = csv.DictWriter(f2, fieldnames= ["first", "last", "house"])
+    writer.writerow({"first": "first", "last": "last", "house": "house"})
+    for row in output_data:
+        writer.writerow({"first": row["first"], "last": row["last"], "house": row["house"]})
