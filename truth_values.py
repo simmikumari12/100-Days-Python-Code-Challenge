0deee9bdbf091308c61021c55dbc18da051ea923
Simmi Kumari <simmikumari942@gmail.com>
2025-02-15 21:50:59 -0500

    Coded truth_values.py
    This file gives the truth value for conjunction, disjunction, exclusive or,
    conditional statement, and Biconditional when the truth value for the individual preposition is provided.

--- truth_values.py
+++ truth_values.py
@@ -1,5 +1,45 @@
 def main():
-    pass
+    
+    p = input("Truth value for p: ").title()
+    q = input("Truth value for q: ").title()
+    print("p and q:",conjunction(p,q))
+    print("p or q:", disjunction(p,q))
+    print("p xor q:", exclusive_or(p,q))
+    print("p --> q:", conditional(p,q))
+    print("p <--> q:", bi_conditional(p,q))
+
+def conjunction(a,b):
+    if a == True and b == True:
+        return True
+    else:
+        return False
+
+def  disjunction(a,b):
+    if a == False and b == False:
+        return False
+    else:
+        return True
+
+def exclusive_or(a,b):
+    if a != b:
+        return True
+    else:
+        return False
+    
+def conditional(a,b):
+    if a == True and b == False:
+        return True
+    else:
+        return False
+    
+def bi_conditional(a,b):
+    if a == b:
+        return True
+    else:
+        return False
+
+    
+
 
 if __name__ == "__main__":
     main()
