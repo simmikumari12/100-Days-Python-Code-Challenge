963afb813494d810ccc5d504090a05cfedf24ccb
Simmi Kumari <simmikumari942@gmail.com>
2025-03-09 17:45:22 -0400

    Update lines.py

--- lines.py
+++ lines.py
@@ -1,6 +1,16 @@
 import sys
 
 def main():
+
+    l = len(sys.argv)
+    if l > 2:
+        sys.exit("Too many command line arguments")
+    elif l < 2:
+        sys.exit("Too few command line arguments")
+    
+    if not (sys.argv[1].endswith(".py")):
+        sys.exit("Not a valid python file.")
+
     
     print(num_lines(sys.argv[1]))
 
