0f56d7db9c3275283d988865671745bdd162ac64
Simmi Kumari <simmikumari942@gmail.com>
2025-03-09 21:15:44 -0400

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,3 +1,21 @@
 import sys
 import tabulate
 import csv
+
+def main():
+    valid_input()
+    print(my_table(sys.argv[1]))
+
+def valid_input():
+    l = len(sys.argv)
+    if l > 2:
+        sys.exit("Too many command line arguments")
+    if l < 2:
+        sys.exit("Too Few Command Line Arguments")
+
+    
+
+
+
+def my_table(my_file):
+    ...
